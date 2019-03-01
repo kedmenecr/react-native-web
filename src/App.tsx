53bd1c09200886c179472f61d934851510fc8531
Roberto Kedmenec <rexerd96@gmail.com>
2019-03-01 06:58:40 +0100

    Init CRA ts

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>
+        Hello from react native web!
+      </Text>
+    </View>
+  );
+}
+
+export default App;
